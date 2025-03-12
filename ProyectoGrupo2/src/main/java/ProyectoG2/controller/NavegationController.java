ce76109e8ce9cfee6494e4a8258922bb3531fa64
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-03-12 00:59:13 -0600

    update
    update

--- ProyectoGrupo2/src/main/java/ProyectoG2/controller/NavegationController.java
+++ ProyectoGrupo2/src/main/java/ProyectoG2/controller/NavegationController.java
@@ -14,10 +14,29 @@ public class NavegationController {
    }
 
    @GetMapping("/sede") // Nueva ruta para vista.html
-   public String mostrarVista() {
+   public String mostrarSede() {
        return "sede"; // Esto buscará vista.html en templates/
    }
 
+   @GetMapping("/promociones") // Nueva ruta para vista.html
+   public String mostrarPromociones() {
+       return "promociones"; // Esto buscará vista.html en templates/
+   }
+
+   @GetMapping("/contacto") // Nueva ruta para vista.html
+   public String mostrarContacto() {
+       return "contacto"; // Esto buscará vista.html en templates/
+   }
+
+   @GetMapping("/home") // Nueva ruta para vista.html
+   public String mostrarHome() {
+       return "home"; // Esto buscará vista.html en templates/
+   }
+   
+   @GetMapping("/perfil") // Nueva ruta para vista.html
+   public String mostrarPerfil() {
+       return "perfil"; // Esto buscará vista.html en templates/
+   }
 
 
 
