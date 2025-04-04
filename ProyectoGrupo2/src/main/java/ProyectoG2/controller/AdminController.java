000b0e269bd31092ae7dea9e85d0023c63b7f6c0
Santiago Brenes <itssanti_@outlook.com>
2025-04-04 08:53:44 -0600

    Update: Se agrego modulo clases, correciones en home admin, todo: columna de acciones edit,update,new

--- ProyectoGrupo2/src/main/java/ProyectoG2/controller/AdminController.java
+++ ProyectoGrupo2/src/main/java/ProyectoG2/controller/AdminController.java
@@ -1,8 +1,13 @@
 package ProyectoG2.controller;
 
+import ProyectoG2.domain.Blog;
+import ProyectoG2.domain.Clase;
 import ProyectoG2.domain.Tienda;
+import ProyectoG2.domain.Usuario;
+import ProyectoG2.service.BlogService;
 import ProyectoG2.service.ClaseService;
 import ProyectoG2.service.TiendaService;
+import ProyectoG2.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,8 +25,26 @@ public class AdminController {
     @Autowired
     private TiendaService tiendaService;
 
+    @Autowired
+    private ClaseService claseService;
+
+    @Autowired
+    BlogService blogService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("")
-    public String homeAdmin() {
+    public String homeAdmin(Model model) {
+        List<Tienda> productos = tiendaService.listarProductos();
+        List<Clase> clases = claseService.getClases();
+        List<Blog> blogs = blogService.getAllBlogs();
+        //List<Usuario> usuarios = usuarioService.getUsuarios();
+
+        model.addAttribute("total_productos", productos.size());
+        model.addAttribute("total_clases", clases.size());
+        model.addAttribute("total_blogs", blogs.size());
+        //model.addAttribute("total_usuarios", usuarios.size());
         return "private/admin/home";
     }
 
@@ -52,5 +75,26 @@ public class AdminController {
         return "private/admin/productos/lista";
     }
 
-    ;
+    @GetMapping("clases")
+    public String getListaClases(Model model) {
+        List<Clase> clases = claseService.getClases();
+
+        List<List<String>> items = new ArrayList<>();
+
+        for (Clase clase : clases) {
+            List<String> item = new ArrayList<>();
+            item.add(Integer.toString(
+                    clase.getIdClase()
+            ));
+            item.add(clase.getNombre_clase());
+            item.add(clase.getTipo());
+            item.add(clase.getDificultad());
+            item.add(clase.getInstructor());
+            items.add(item);
+        }
+
+        model.addAttribute("clases", items);
+        model.addAttribute("total_clases", clases.size());
+        return "private/admin/clases/lista";
+    }
 }
