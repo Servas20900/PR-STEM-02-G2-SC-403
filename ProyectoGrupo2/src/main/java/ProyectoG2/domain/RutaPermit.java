f362d7150dd4abf50a79da33ec2381148303c14f
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-04-13 23:41:06 -0600

    RutaPermit.java

---
+++ ProyectoGrupo2/src/main/java/ProyectoG2/domain/RutaPermit.java
@@ -0,0 +1,19 @@
+package ProyectoG2.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "ruta_permit")
+public class RutaPermit implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRuta;
+
+    private String patron; // ejemplo: /login, /registro, /css/**, etc.
+}
