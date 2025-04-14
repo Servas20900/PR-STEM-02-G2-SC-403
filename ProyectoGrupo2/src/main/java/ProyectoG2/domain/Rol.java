2311739e5550ce71779aa8ce1cdd5791d7c4cff0
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-04-13 23:38:37 -0600

    Rol.java,Usuario.java
    update

--- ProyectoGrupo2/src/main/java/ProyectoG2/domain/Rol.java
+++ ProyectoGrupo2/src/main/java/ProyectoG2/domain/Rol.java
@@ -1,42 +1,31 @@
 package ProyectoG2.domain;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.io.Serializable;
-import java.util.List;
 import lombok.Data;
 import javax.validation.constraints.NotEmpty;
 
 
 @Entity
 @Data
-@Table(name="usuario")
-public class Usuario implements Serializable {
+@Table(name = "rol")
+public class Rol implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idUsuario;
+    private Long idRol;
 
     @NotEmpty
-    private String username;
-
-    @NotEmpty
-    private String password;
-
     private String nombre;
-    private String apellidos;
-    private String correo;
-    private String telefono;
 
-    @OneToMany
-    @JoinColumn(name="id_usuario")
-    private List<Rol> roles;
+    @Column(name = "id_usuario")
+    private Long idUsuario;
 }
