4a222ef516b4242be4182629405ab6cb38f31e46
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-04-23 11:34:43 -0600

    Correciones en usuario service y ajustes en css de pago y ajustes

--- ProyectoGrupo2/src/main/java/ProyectoG2/service/UsuarioService.java
+++ ProyectoGrupo2/src/main/java/ProyectoG2/service/UsuarioService.java
@@ -1,32 +1,27 @@
 package ProyectoG2.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import ProyectoG2.domain.Usuario;
 public interface UsuarioService {
     
-    // Se obtiene un listado de usuarios en un List
     public List<Usuario> getUsuarios();
     
-    // Se obtiene un Usuario, a partir del id de un usuario
     public Usuario getUsuario(Usuario usuario);
     
-    // Se obtiene un Usuario, a partir del username de un usuario
     public Usuario getUsuarioPorUsername(String username);
 
-    // Se obtiene un Usuario, a partir del username y el password de un usuario
     public Usuario getUsuarioPorUsernameYPassword(String username, String password);
     
-    // Se obtiene un Usuario, a partir del username o el correo del usuario
     public Usuario getUsuarioPorUsernameOCorreo(String username, String correo);
+
+  
     
+
     public void save(Usuario usuario);
-    // Se elimina el usuario que tiene el id pasado por parámetro
+
     public void delete(Usuario usuario);
 
 
-    // //para auth
-    // Usuario findByUsername(String username);
-
-    // boolean existsByUsernameOrCorreo(String username, String correo);
 }
